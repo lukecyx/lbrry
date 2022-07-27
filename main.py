16df506096dbf1bc93e78ce73b02ff08c3605d10
Luke Holmes <lukemartynholmes@gmail.com>
2022-07-27 19:49:03 +0000

    feat: Add server_check endpoint.

--- main.py
+++ main.py
@@ -1,8 +1,21 @@
 from fastapi import FastAPI
 
+from http import HTTPStatus
+
+
 app = FastAPI()
 
 
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
+
+
+@app.get("/check_health")
+def check_health() -> int:
+    """Check the health of the app.
+
+    Only return 200, if app is down we'll get a 502.
+    """
+
+    return HTTPStatus.OK.value
